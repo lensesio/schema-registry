9e5c4ef5cdeef56501874d8d697aab3f126470bf
Rollulus <roelboel@xs4all.nl>
2016-06-17 20:12:22 +0200

    Add versions command

---
+++ schemaregistrycli/cmd/versions.go
@@ -0,0 +1,29 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var versionsCmd = &cobra.Command{
+	Use:   "versions",
+	Short: "",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			log.Fatalf("expected 1 argument")
+		}
+		client := assertClient(registryUrl)
+		vers, err := client.Versions(args[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%v\n", vers)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(versionsCmd)
+}
