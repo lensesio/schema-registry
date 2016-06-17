f1a5c31b7725a76e6c15bf3752de05ce69193dcd
Rollulus <roelboel@xs4all.nl>
2016-06-17 21:18:04 +0200

    Add `exists` command

---
+++ schemaregistrycli/cmd/exists.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var existsCmd = &cobra.Command{
+	Use:   "exists",
+	Short: "",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			log.Fatalf("expected 1 argument")
+		}
+		isreg, sch, err := assertClient(registryUrl).IsRegistered(args[0], stdinToString())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("exists: %v\n", isreg)
+		if !isreg {
+			return
+		}
+		fmt.Printf("id: %d\n", sch.Id)
+		fmt.Printf("version: %d\n", sch.Version)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(existsCmd)
+}
