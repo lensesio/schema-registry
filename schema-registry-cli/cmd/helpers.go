c527246c46993701d9080593519b4094da786b85
Chuck Kato <chuck.kato@coursehero.com>
2019-07-10 09:57:12 -0700

    Support basic authentication

--- schema-registry-cli/cmd/helpers.go
+++ schema-registry-cli/cmd/helpers.go
@@ -85,7 +85,18 @@ func getConfig(subj string) error {
 }
 
 func assertClient() *schemaregistry.Client {
-	c, err := schemaregistry.NewClient(viper.GetString("url"))
+	var c *schemaregistry.Client
+	var err error
+	if viper.GetString("basic_auth_user") != "" && viper.GetString("basic_auth_pass") != "" {
+		// Use Basic Authentication
+		b := &BasicAuthCredential{
+			Username: viper.GetString("basic_auth_user"),
+			Password: viper.GetString("basic_auth_pass"),
+		}
+		c, err = schemaregistry.NewClient(viper.GetString("url"), schemaregistry.UsingClient(b.GetClient()))
+	} else {
+		c, err = schemaregistry.NewClient(viper.GetString("url"))
+	}
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
